dfaafcd6b13d3e9cd3089e5f5bac20328c7624e1
GertonsonR <54896195+GertonsonR@users.noreply.github.com>
2019-09-16 20:12:14 -0500

    first 3 labs

---
+++ OutputLab/src/output/asciiart.java
@@ -0,0 +1,21 @@
+package output;
+
+public class asciiart {
+
+	public static void main(String[] args) {
+		System.out.println("Clayton Gertonson \t  9-15-19 \t Period 1\n\n" );
+		System.out.println("I'll be drawing a bunny." );
+		System.out.println("\n\n\n\n" );
+
+
+		System.out.println("               (\\_(\\                " );
+		System.out.println("               (=-.-)               " );
+		System.out.println("               o_(\")(\")                  " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ OutputLab/src/output/asciibox.java
@@ -0,0 +1,27 @@
+package output;
+
+public class asciibox {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Clayton Gertonson \t  9-12-19 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+	}
+}
