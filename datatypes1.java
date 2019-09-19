3d06b1b4e18cb4911edb73d957cdc03965221ccb
GertonsonR <54896195+GertonsonR@users.noreply.github.com>
2019-09-19 08:18:48 -0500

    Add files via upload

---
+++ datatypes1.java
@@ -0,0 +1,17 @@
+package advancedCS;
+import java.util.Scanner;
+public class datatypes1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner keyboard = new Scanner(System.in);
+		int intOne, intTwo;
+		System.out.print("Enter an integer :: ");
+		intOne = keyboard.nextInt();
+		System.out.print("Enter an integer :: ");
+		intTwo = keyboard.nextInt();
+		System.out.println("integer one = " + intOne);
+		System.out.println("integer two = " + intTwo);
+	}
+
+}
